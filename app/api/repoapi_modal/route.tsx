777ff8a2fe6b23e3db2bbcbda7cbf87a38bc4e2f
Kyle <kyle.tz.cui@gmail.com>
2023-11-26 16:38:05 -0800

    okk

--- app/api/repoapi_modal/route.tsx
+++ app/api/repoapi_modal/route.tsx
@@ -2,15 +2,16 @@ import { NextResponse } from "next/server";
 
 import axios from 'axios';
 
-export async function GET(req: Request, res: Response) {
+export async function POST(req: Request) {
     try {
-        
         const body = await req.json();
+        console.log("Received: ")
+        console.log(body)
         const response = await axios({
             method: 'get',
-            url: 'https://external-api.com/data',
+            url: '',
             headers: {
-              'Authorization': `Bearer ${process.env.YOUR_SECRET_API_KEY}`,
+              'Authorization': `Bearer ${process.env.REPOAPI_MODAL_KEY}`,
               'Content-Type': 'application/json',
             },
             data: body,
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,92 @@
 "use client";
 
 import React, { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from '@/components/ui/input'
 
 import axios from 'axios';
 
-interface UserInputData {
-  repo_link: string;
-  apis: string;
-}
-
-const callRepoApi_Modal = async (userInputData: UserInputData) => {
-  console.log(userInputData.repo_link)
-  try {
-    const response = await axios.post('/api/repoapi_modal', userInputData);
-    const data = response.data;
-    // Update your UI with this data
-  } catch (error) {
-    console.error(error);
-  }
-}
+const formSchema = z.object({
+  repo_link: z.string(),
+  apis: z.string(),
+})
 
 export default function Home() {
-  const [formData, setFormData] = useState({
-    repo_link: '',
-    apis: '',
-  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      repo_link: "",
+      apis: "",
+    }
+  })
 
-  const updateFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({...formData, [name]: value});
-  };
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log("Submitted!")
+      const userInput = {
+        repo_link: values.repo_link,
+        apis: values.apis,
+      };
+      const response = await axios.post('/api/repoapi_modal', userInput);
+      //const data = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div>
-      <body className="bg-white text-gray-800">
-          <div className="container mx-auto px-4 py-8">
-
-              <form>
-                <input type="text" name="repo_link" value={formData.repo_link} onChange={updateFormData}/>
-                <input type="text" name="apis" value={formData.apis} onChange={updateFormData}/>
-                <button onClick={() => callRepoApi_Modal(formData)}>Generate</button>
-              </form>
-          </div>
-      </body>
+      <h1 className="space-y-8">Repi</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="repo_link"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>GitHub Repo Link</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://github.com/..." {...field} />
+                </FormControl>
+                <FormDescription>
+                  Link to GitHub repository...
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="apis"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>APIs</FormLabel>
+                <FormControl>
+                  <Input placeholder="file_name.function..." {...field} />
+                </FormControl>
+                <FormDescription>
+                  APIs
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  );
-};
+    
+  )
+}
